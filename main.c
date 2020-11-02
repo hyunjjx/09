914f8fd97cbb947cb2b8731c825b29d839bb7720
jeongxhj <55428319+jeongxhj@users.noreply.github.com>
2020-11-02 23:34:12 +0900

    02-1

--- main.c
+++ main.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define SIZE 5
 
 int main(int argc, char *argv[]) {
-	
-	//array declarlation
+
 	int i;
-	int grade[5];
+	int grade[SIZE];
+	printf("5명의 점수를 입력하세요.\n");
 	
-	//value input
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
-	
-	//print
-	for(i=0;i<5;i++)
-		printf("student %i's grade : %i\n", i, grade[i]);
+	for(i=0;i<SIZE;i++)
+		scanf("%d", &grade[i]);
+		
+	for(i=0;i<SIZE;i++)
+		printf("grade[%d] = %d \n",i,grade[i]);
+		
 	return 0;
 }
